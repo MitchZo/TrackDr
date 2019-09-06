b544daea2fe9498c6b0f3e540916a6a01188f142
mitch <mitchell.zolynsky@gmail.com>
2019-09-06 11:35:17 -0400

    create initial project
    ran scaffolding, created db, and updated appsettings.json

---
+++ TrackDr/Models/SavedDoctors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrackDr.Models
+{
+    public partial class SavedDoctors
+    {
+        public int Id { get; set; }
+        public string DrList { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
