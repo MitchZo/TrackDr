504509fbef0346b36be70575074023bfdbd5f0ce
mitch <mitchell.zolynsky@gmail.com>
2019-09-07 18:18:01 -0400

    Update DatabaseController.cs
    added dependency injection for the database and the API key

--- TrackDr/Controllers/DatabaseController.cs
+++ TrackDr/Controllers/DatabaseController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TrackDr.Models;
 
 namespace TrackDr.Controllers
 {
     public class DatabaseController : Controller
     {
+        private readonly TrackDrDbContext _context;
+        private readonly IConfiguration _configuration;
+        public DatabaseController(TrackDrDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
         public IActionResult Index()
         {
             return View();
